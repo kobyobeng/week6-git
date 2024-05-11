ae72e907d63bb098b8c56840ff23c1602b24dc66
Kobby Obeng <kobbyobeng@kobbys-air.home>
2024-05-11 16:04:53 +0100

    hgsd

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_lightsail_instance" "server1" {
     name = "dev-server-2"
     blueprint_id = "ubuntu_18_04"
     bundle_id = "medium_1_0"
-    availability_zone = "us-east-1a"
+    availability_zone = "${var.REGION}a"
     user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "ENVIRONMENT"{
+    
+} 
+
+variable "REGION"{
+    
+} 
+
